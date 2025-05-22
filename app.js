323ab9f6ba11b339ba95ff077012e9c0d55237f4
Rafael Santana Rodrigues <138138154+RafaelSR44@users.noreply.github.com>
2025-05-22 09:21:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const alunosRoutes = require('./routes/alunos');
-const bodyParser = require('body-parser');
 const professoresRoutes = require('./routes/professores');
+const cursosRoutes = require('./routes/cursos');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 app.set('view engine', 'ejs');
@@ -14,6 +15,7 @@ app.use(express.static('public'));
 
 app.use('/alunos', alunosRoutes);
 app.use('/professores', professoresRoutes);
+app.use('/cursos', cursosRoutes);
 
 app.get('/', (req, res) => {
   res.redirect('/alunos');
@@ -24,5 +26,3 @@ app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
-const cursosRoutes = require('./routes/cursos');
-app.use('/cursos', cursosRoutes);
